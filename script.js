bbf5559848c8e75483c8dd1346321884405973bc
tomerglix <tomerglix@gmail.com>
2013-12-09 10:59:08 +0200

    trying to fix spinner

--- script.js
+++ script.js
@@ -541,21 +541,22 @@
 					left: 'auto' // Left position relative to parent in px
                 };
                             
-				var HugeSpinnerOpts = {
-				  	lines: 17, // The number of lines to draw
-				  	length: 40, // The length of each line
-				  	width: 14, // The line thickness
-				  	radius: 60, // The radius of the inner circle
-				  	corners: 1, // Corner roundness (0..1)
-				  	rotate: 0, // The rotation offset
-				  	direction: 1, // 1: clockwise, -1: counterclockwise
-				  	color: '#000', // #rgb or #rrggbb or array of colors
-				  	speed: 1, // Rounds per second
-				  	trail: 60, // Afterglow percentage
-				  	shadow: false, // Whether to render a shadow
-				  	hwaccel: false, // Whether to use hardware acceleration
-				  	className: 'spinner', // The CSS class to assign to the spinner
-				  	zIndex: 2e9, // The z-index (defaults to 2000000000)
-				  	top: 'auto', // Top position relative to parent in px
-				  	left: 'auto' // Left position relative to parent in px
-				};                        
+				var HugeSpinnerOpts = 
+				{
+				  lines: 13, // The number of lines to draw
+				  length: 13, // The length of each line
+				  width: 6, // The line thickness
+				  radius: 13, // The radius of the inner circle
+				  corners: 1, // Corner roundness (0..1)
+				  rotate: 0, // The rotation offset
+				  direction: 1, // 1: clockwise, -1: counterclockwise
+				  color: '#000', // #rgb or #rrggbb or array of colors
+				  speed: 0.9, // Rounds per second
+				  trail: 58, // Afterglow percentage
+				  shadow: false, // Whether to render a shadow
+				  hwaccel: false, // Whether to use hardware acceleration
+				  className: 'spinner', // The CSS class to assign to the spinner
+				  zIndex: 2e9, // The z-index (defaults to 2000000000)
+				  top: 'auto', // Top position relative to parent in px
+				  left: 'auto' // Left position relative to parent in px
+				};                 
