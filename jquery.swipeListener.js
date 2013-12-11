d610ad7b2a1abf3daddce65fb6010094cd0dc51f
tomerglix <tomerglix@gmail.com>
2013-12-11 15:56:58 +0200

    new picker

--- jquery.swipeListener.js
+++ jquery.swipeListener.js
@@ -9,10 +9,10 @@
 	$.fn.swipeListener = function(option){
 		var config = {
 			duration: 1000, // within this period
-			minX: 20, // swipe L/R if touch position moved more than this horizontally
+			minX: 50, // swipe L/R if touch position moved more than this horizontally
 			minY: 20, // swipe U/D if touch position moved more than this vertically
-			swipeUp: undefined,   // callback function invoked when swipe up
-			swipeDown: undefined, // or swipe down,
+			//swipeUp: undefined,   // callback function invoked when swipe up
+			//swipeDown: undefined, // or swipe down,
 			swipeLeft: undefined, // or swipe left,
 			swipeRight: undefined // or swipe right
 		};
@@ -74,14 +74,14 @@
 							config.swipeRight();
 						}
 						
-					} else if (Math.abs(diffY) > config.minY) {
+					}/* else if (Math.abs(diffY) > config.minY) {
 						
 						if (diffY > 0 && config.swipeUp) {
 							config.swipeUp();
 						} else if (config.swipeDown) {
 							config.swipeDown();
 						}
-					}
+					}*/
 				}
 				
 				start = stop = undefined;
