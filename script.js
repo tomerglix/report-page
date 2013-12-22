356a01216ece0e5859ba45d5143747c975770c79
tomerglix <tomerglix@gmail.com>
2013-12-22 13:03:58 +0200

    alltogether

--- script.js
+++ script.js
@@ -209,6 +209,10 @@
 		                actionResult = uploadIframe.document.body.innerHTML;
 		            }
 					
+					
+					alert(actionResult);
+					
+					
 					if (form.id=='SOSForm')
 					{
 						CheckActionResult(actionResult,5,SOSSuccessStr,SOSFailStr);
@@ -220,6 +224,7 @@
 					}
 					else if (form.id=='activationWaitForm')
 					{
+						alert(url);
 						var res=CheckActionResult(actionResult,1,checkActivationSuccessStr,checkActivationFailStr,1);
 						if (res==true)
 						{			
@@ -234,11 +239,8 @@
 							
 							
 							var n=actionResult.indexOf(':');
-							alert(actionResult);
 							var userIdStr=actionResult.substring(0,n);
-							alert(userIdStr);
 							userId=parseInt(userIdStr);
-							alert(userId);
 							localStorage.setItem('contactsCounter',0);
 							localStorage.setItem('userId',userId);
 							localStorage.setItem('userStatus',2);
@@ -253,57 +255,6 @@
 					}
 				}
 				
-				function fileUpload(form, action_url) 
-				{
-				    // Create the iframe...
-				    var iframe = document.createElement("iframe");
-				    iframe.setAttribute("id", "upload_iframe");
-				    iframe.setAttribute("name", "upload_iframe");
-				    iframe.setAttribute("width", "0");
-				    iframe.setAttribute("height", "0");
-				    iframe.setAttribute("border", "0");
-				    iframe.setAttribute("style", "width: 0; height: 0; border: none;");
-				 
-				    // Add to document...
-				    form.parentNode.appendChild(iframe);
-				    window.frames['upload_iframe'].name = "upload_iframe";
-				 
-				    iframeId = document.getElementById("upload_iframe");
-				 
-				    // Add event...
-				    var eventHandler = function () {
-				 
-				            if (iframeId.detachEvent) 
-				            	iframeId.detachEvent("onload", eventHandler);
-				            else 
-				            	iframeId.removeEventListener("load", eventHandler, false);
-				 
-				            // Message from server...
-				            if (iframeId.contentDocument) {
-				                actionResult = iframeId.contentDocument.body.innerHTML;
-				            } else if (iframeId.contentWindow) {
-				                actionResult = iframeId.contentWindow.document.body.innerHTML;
-				            } else if (iframeId.document) {
-				                actionResult = iframeId.document.body.innerHTML;
-				            }
-							
-				            // Del the iframe...
-				            setTimeout('iframeId.parentNode.removeChild(iframeId)', 250);
-				        };
-				    if (iframeId.addEventListener) iframeId.addEventListener("load", eventHandler, true);
-				    if (iframeId.attachEvent) iframeId.attachEvent("onload", eventHandler);
-				 
-				    // Set properties of form...
-				    form.setAttribute("target", "upload_iframe");
-				    form.setAttribute("action", action_url);
-				    form.setAttribute("method", "post");
-				 
-				    // Submit the form...
-				    form.submit();
-				 
-				 	
-				    //document.getElementById(div_id).innerHTML = "Uploading...";
-				}
                
                 function AddParmameterToURL(url,param,value)
                 {
@@ -1202,7 +1153,23 @@
 					
 				}
 				
-			
+				function WaitForActivationOnLoad()
+				{
+					uploadIframe=document.getElementById('uploadIframe');
+					GenerateCheckActivationUrl();
+					CheckActivation();
+				}
+				
+				function CheckActivation()
+				{
+					SendUrl(activationWaitForm);
+										
+				}
+				function GenerateCheckActivationUrl()
+				{
+					url="http://62.0.66." + serverNum +":8080/checkAgentActivation.do?";
+					url+='agentId='+localStorage.getItem('userId');
+				}			
 		
 				
 /******************** storage format *****************************
